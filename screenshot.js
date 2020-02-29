aff49a44c68e871c11718a4e95580d650213e78a
Sutan Nasution <sutan.gnst@gmail.com>
2020-03-01 02:24:07 +0700

    tidy up

--- screenshot.js
+++ screenshot.js
@@ -1,42 +1,40 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer')
 
-async function getScreenshot(url, type = "png") {
-  console.log({ url, type });
+async function getScreenshot(url, type = 'png') {
+  console.log({ url, type })
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox"]
-  });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  await autoScroll(page);
+    args: ['--no-sandbox']
+  })
+  const page = await browser.newPage()
+  await page.goto(url)
+  await autoScroll(page)
   const file = await screenshotAyat(page, type)
-  await browser.close();
-  return file;
+  await browser.close()
+  return file
 }
 
 async function screenshotAyat(page, type) {
   const ayat = await page.$('#bismillah + div')
-  return ayat.screenshot({ type });
-  
+  return ayat.screenshot({ type })  
 }
 
 async function autoScroll(page) {
   await page.evaluate(async () => {
-    await new Promise((resolve, reject) => {
-      var totalHeight = 0;
-      var distance = 100;
-      var timer = setInterval(() => {
-        var scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-
-        if (totalHeight >= scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 100);
-    });
-  });
+    await new Promise(resolve => {
+      let totalHeight = 0
+      const 
+        distance = 100,
+        timer = setInterval(() => {
+          const scrollHeight = document.body.scrollHeight
+          window.scrollBy(0, distance)
+          totalHeight += distance
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer)
+            resolve()
+          }
+        }, 100)
+    })
+  })
 }
 
-module.exports = getScreenshot;
+module.exports = getScreenshot
