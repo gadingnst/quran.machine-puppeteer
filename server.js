846d3a1d61a747a7b961be568e19a4e4a8540f14
Sutan Nasution <sutan.gnst@gmail.com>
2020-03-01 08:15:55 +0700

    update

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 /* Setting things up. */
+const express = require('express')
+const instagram = require('./src/instagram')
+
+const server = express()
+const port = process.env.PORT || 9600
+
+server.use(express.static('public'))
+
+server.listen(port, () => {
+    console.info('Bot running in port:', port)
+})
