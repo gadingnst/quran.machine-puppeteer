ae50c65637572e0782992e1e95c8409f3db58b4f
Gading <sutan.gnst@gmail.com>
2020-08-19 23:49:58 +0700

    JSON response

--- src/api.ts
+++ src/api.ts
@@ -8,13 +8,21 @@ route.get('/publish', async (req, res) => {
   const { secret } = req.query
 
   if (secret !== SECRET_CODE) {
-    return res.status(403).send('Access Forbidden')
+    return res.status(403).send({
+      code: 403,
+      message: 'Access Forbidden.',
+      error: false
+    })
   }
 
   const runTask = async (): Promise<any> => {
     try {
       await publishPost()
-      return res.status(200).send('Success Publishing Post!')
+      return res.status(200).send({
+        code: 200,
+        message: 'Success Publishing Post!',
+        error: false
+      })
     } catch (reason) {
       const { response } = reason
       
@@ -27,8 +35,11 @@ route.get('/publish', async (req, res) => {
       }
       
       console.error('Error on publishing surah:', reason)
-      return res.status(500)
-        .send('Something went wrong, try again later.')
+      return res.status(500).send({
+        code: 500,
+        message: 'Something went wrong, try again later.',
+        error: reason
+      })
     }
   }
   
