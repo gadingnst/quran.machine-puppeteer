ab4a3d414ecade04a55f07da99ad4f94877cccef
Gading <sutan.gnst@gmail.com>
2020-08-24 21:25:38 +0700

    revise

--- src/api.ts
+++ src/api.ts
@@ -6,8 +6,6 @@ const route = Router()
 
 route.get('/publish', async (req, res) => {
   const { secret } = req.query
-  let settled = false
-
   if (secret !== SECRET_CODE) {
     return res.status(403).send({
       code: 403,
@@ -17,39 +15,43 @@ route.get('/publish', async (req, res) => {
   }
 
   /* prevent 502 in vercel, request timeout on 10 seconds */
+  let processing = true
   setTimeout(() => {
-    return !settled && res.status(301).send({
-      code: 301,
-      message: 'Request timeout! Process still running but request exited.',
-      error: false
-    })
-  }, 9000)
+    if (processing) {
+      processing = false
+      return res.status(301).send({
+        code: 301,
+        message: 'Request timeout! Process still running but request exited.',
+        error: false
+      })
+    } 
+  }, 9250)
 
   try {
     await publishPost()
-    settled = true
-    return res.status(201).send({
+    return processing && res.status(201).send({
       code: 201,
       message: 'Success Publishing Post!',
       error: false
     })
   } catch (reason) {
     const { aspectRatioError, surah, ayat } = reason
-    settled = true
     
     if (aspectRatioError) {
-      return res.status(503).send({
+      return processing && res.status(503).send({
         code: 503,
         message: `Cannot publish surah because aspect ratio not match with instagram. On: Q.S. ${surah}:${ayat}`,
         error: true
       })
     }
     
-    return res.status(500).send({
+    return processing && res.status(500).send({
       code: 500,
       message: 'Something went wrong, try again later.',
       error: reason
     })
+  } finally {
+    processing = false
   }
 
 })
